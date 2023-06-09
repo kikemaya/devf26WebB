28feb8ef1628ddbcfcf983e6ddb22b68722d1921
kikemaya <enrique.mg.cd@gmail.com>
2023-06-08 21:40:38 -0600

    primeras instrucciones de javascript

---
+++ intro-js/js/index.js
@@ -0,0 +1,22 @@
+// SOY UN COMENTARIO DE UNA SOLA LINEA
+
+/*
+  SOY
+  UN COMENTARIO
+  DE VARIAS LINEAS
+*/
+
+// OBTENER MENSAJES EN LA CONSOLA DE DESARROLLO
+console.log('"Hola desde el archivo JS"');
+console.warn("Hola soy una alerta");
+console.error("Hola soy un error");
+
+// SALIDA DE DATOS EN EL DOCUMENTO DE LA PAGINA
+document.write("Hola mundo!");
+
+// MENSAJES DE ALERTA EN UN POP UP
+alert("¡ALERTA!");
+
+// ENTRADA DE DATOS
+var nombre = prompt("Por favor, dime tu nombre");
+alert("Hola " + nombre);
