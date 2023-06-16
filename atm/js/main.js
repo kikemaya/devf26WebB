15d6f4c467b606901e18822f4ef16e5223c619e1
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-06-15 20:20:30 -0600

    feat: finish atm login

--- atm/js/main.js
+++ atm/js/main.js
@@ -1,32 +1,48 @@
-const formulario = document.querySelector('#select-account')
+const formulario = document.querySelector("#select-account");
+const select = document.querySelector("select");
+const body = document.querySelector("body");
 
+const cuentas = [
+  { id: 10, nombre: "Victor", saldo: 900, pin: "0000" },
+  { id: 24, nombre: "Enrique", saldo: 990, pin: "1234" },
+  { id: 11, nombre: "Rodrigo", saldo: 200, pin: "8976" },
+];
 
-const cuenta = [
-  {id: 10, nombre: "Victor", saldo: 900, pin: '0000'}, 
-  {id: 24, nombre: "Enrique", saldo: 990, pin: '1234'}, 
-  {id: 11, nombre: "Rodrigo", saldo: 200, pin: '8976'}
-]
+const validatePIN = (id, pin) => {
+  let selected = cuentas.filter((cuenta) => {
+    return cuenta.id === parseInt(id);
+  });
+  if (selected[0].pin === pin) {
+    alert("Bienvenido");
+    // body.innerHTML= ''
+    location.href = "/opciones/";
+  } else {
+    alert("Intenta nuevamente");
+  }
+};
 
-formulario.addEventListener('submit', (event)=> {
-  event.preventDefault()
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let user_id = event.target["cuenta"].value;
+
   let pin = prompt("Ingrese su pin", "");
   if (pin != null) {
-    if (pin === '0000'){
-      console.log('Correcto')
-    } else {
-      console.log('Incorrecto')
-    }
+    validatePIN(user_id, pin);
   }
   // console.log(event.target[0].value)
-})
+});
 
+cuentas.forEach((cuenta) => {
+  let option = document.createElement("option");
+  option.value = cuenta.id;
+  option.innerText = cuenta.nombre;
+  select.appendChild(option);
+});
 
-
-// [] <- Array 
+// [] <- Array
 // {} <- Object
 
-
-// Pseudocódigo 
+// Pseudocódigo
 /*
 1. Seleccionar la cuenta 
 2. Cuando se seleccione una cuenta se solicitará el pin 
